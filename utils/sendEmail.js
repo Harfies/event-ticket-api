42ce6e8252fdf5bcb42b78d8f46b24ddd80a4b25
Harfies <akinsolaafeez82@gmail.com>
2026-05-13 15:14:11 +0100

    update mailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,33 +1,9 @@
 const nodemailer = require("nodemailer");
 
-/*const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-const sendEmail = async (to, subject, text) => {
-  try {
-    console.log("Sending email to:", to);
-
-    const info = await transporter.sendMail({
-      from: `"Event Tickets 🎟️" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text,
-    });
-
-    console.log("Email sent ✅:", info.response);
-  } catch (error) {
-    console.log("Email failed ❌:", error.message);
-  }
-};
-*/
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
